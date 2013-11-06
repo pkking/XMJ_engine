a351cb478b4a82d5271c845b0ade7fb3f09bcdfe
lichaoran <409624189@qq.com>
2013-11-06 20:06:38 +0800

    使用epoll 暂定并发上限100
    Signed-off-by: lichaoran <409624189@qq.com>

--- http.c
+++ http.c
@@ -1,36 +1,42 @@
 #include "request.h"
-#include <sys/select.h>
 #include <netinet/in.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/time.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
 
-//信号处理
-/*
-static void sig_chld(int signo);
-static void sig_chld(int signo) 
+#define 		MAX_FD			100
+
+int setnonblocking(int sock)
 {
-    pid_t   pid; 
-    int     stat; 
-			    
-    while((pid = waitpid(-1, &stat, WNOHANG)) > 0)
-    {   
-        printf("child %d terminated\n", pid); 
-    }   
-				    
-    return; 
-} 
-*/
+	    int opts;
+	    opts=fcntl(sock,F_GETFL);
+	    if(opts<0)
+        {
+		   fprintf(stderr,"fcntl(sock,GETFL)");
+		   return -1;
+		}
+		opts = opts|O_NONBLOCK;
+		if(fcntl(sock,F_SETFL,opts)<0)
+		{
+			fprintf(stderr,"fcntl(sock,SETFL,opts)");
+		     return -1;
+		}
+		return 0;
+}
+
 int main(int argc, char **argv)
 {
-//	signal(SIGCHLD, sig_chld);
-	int listenfd, connfd, port, client[FD_SETSIZE],maxi,i,max_fd;
+	int listenfd, epollfd, connfd, port, i;
 	socklen_t  clientlen;
-	fd_set read_set;
 	int nready;
 	struct sockaddr_in clientaddr;
+	struct epoll_event ev, events[MAX_FD];
+
 	request_t *q=(request_t *)malloc(sizeof(request_t));
 
 	if(argc <2)
@@ -47,63 +53,49 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	FD_ZERO(&read_set);
-	FD_SET(listenfd, &read_set);
-	max_fd=listenfd;
-	maxi=0;
-
-	for(i=0;i<FD_SETSIZE;i++)
-	{
-		client[i] = -1;
+	epollfd = epoll_create(MAX_FD);
+	if (epollfd == -1) {
+	    fprintf(stderr,"epoll_create error\n");
+	    exit(-1);
 	}
-	while(1)
+	ev.events = EPOLLIN;
+	ev.data.fd = listenfd;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &ev) == -1) {
+	    fprintf(stderr, "epoll_ctl: listen_sock\n");
+	    exit(-1);
+	}
+
+	for(;;)
 	{
 		clientlen = sizeof(clientaddr);
-		nready = select(max_fd+1,&read_set,NULL,NULL,NULL);
-
-		if(FD_ISSET(listenfd,&read_set))
-		{
-			connfd = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
-			if(maxi>FD_SETSIZE)
-			{
-				printf("too many client!\n");
-				exit(-1);
+		nready = epoll_wait(epollfd, events, MAX_FD, -1);//get ready fd
+		if (nready == -1) {
+	        fprintf(stderr,"epoll_wait error !\n");
+	        exit(-1);
+	    }
+		for (i = 0; i < nready; ++i) {
+			if (events[i].data.fd == listenfd) {
+	        connfd = accept(listenfd,
+            (struct sockaddr *) &clientaddr, &clientlen);
+	        if (connfd == -1) {
+	 	       fprintf(stderr,"accept error !\n");
+               exit(-1);
+            }
+            setnonblocking(connfd);
+            ev.events = EPOLLIN | EPOLLET;
+            ev.data.fd = connfd;
+            if (epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &ev) == -1) {
+                fprintf(stderr,"epoll_ctl: conn_sock error !\n");
+	            exit(-1);
+		    }
 			}
-			for(i=0;i<FD_SETSIZE;i++)
-			{
-				if(client[i]<0)
-				{
-					client[i]=connfd;
-					break;
-				}
+			else{
+			recv_request(q,events[i].data.fd);
+			handle_request(q);
+			close(connfd);
 			}
-
-			FD_SET(connfd,&read_set);
-			if(connfd>max_fd)
-				max_fd=connfd;
-			if(i>maxi)
-				maxi=i;
-			if(--nready<=0)//if just the listen socket get ready
-				continue;
-		}
-			//recv_request(q,connfd);	
-			//handle_request(q);
-			//close(connfd)
-		
-		for(i=0;i<FD_SETSIZE;i++)
-		{
-			if(client[i]==-1)
-				continue;
-			if(FD_ISSET(client[i],&read_set))
-			{
-				recv_request(q,client[i]);
-				handle_request(q);
-				close(connfd);
-				FD_CLR(client[i],&read_set);
-				client[i]=-1;
-			}
-			if(--nready<=0)
-				break;
 		}
 	}
+
+	return 0;
 }
